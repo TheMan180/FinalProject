e16fdec4ed9b36aca89942a7d4f11920c0ee5a15
TheMan180 <152086117+TheMan180@users.noreply.github.com>
2024-05-14 22:55:28 +0300

    yandex_gpt.py

--- yandex_gpt.py
+++ yandex_gpt.py
@@ -1,10 +1,27 @@
 import requests
 import logging
-from config import LOGS, MAX_GPT_TOKENS, SYSTEM_PROMPT, FOLDER_ID, IAM_TOKEN
+from config import MAX_GPT_TOKENS, SYSTEM_PROMPT
 from creds import get_creds
 
 
-logging.basicConfig(filename=LOGS, level=logging.ERROR, format="%(asctime)s FILE: %(filename)s IN: %(funcName)s MESSAGE: %(message)s", filemode="w")
+def count_gpt_tokens(messages):
+    iam_token, folder_id = get_creds()
+    url = "https://llm.api.cloud.yandex.net/foundationModels/v1/tokenizeCompletion"
+    headers = {
+        'Authorization': f'Bearer {iam_token}',
+        'Content-Type': 'application/json'
+    }
+    data = {
+        'modelUri': f"gpt://{folder_id}/yandexgpt-lite",
+        "messages": messages
+    }
+    try:
+        return len(requests.post(url=url, json=data, headers=headers).json()['tokens'])
+    except Exception as e:
+        logging.error(e)
+        return 0
+
+
 def ask_gpt(messages):
     iam_token, folder_id = get_creds()
     url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
@@ -30,21 +47,4 @@ def ask_gpt(messages):
         return True, answer, tokens_in_answer
     except Exception as e:
         logging.error(e)
-        return False, "Ошибка при обращении к GPT"
-def count_gpt_tokens(messages):
-    url = "https://llm.api.cloud.yandex.net/foundationModels/v1/tokenizeCompletion"
-    headers = {
-        'Authorization': f'Bearer{IAM_TOKEN}',
-        'Content-Type': 'application/json'
-    }
-    data = {
-        'modelUri': f"gpt://{FOLDER_ID}/yandexgpt-lite",
-        "messages": messages
-    }
-    try:
-        return len(requests.post(url=url, json=data, headers=headers).json()['tokens'])
-    except Exception as e:
-        logging.error(e)
-        return 0
-
-
+        return False, "Ошибка при обращении к GPT",  None
